88b2743c2f1bc7a6927f002c6e08d64fe20ed421
lmt6498 <lmt6498@users.noreply.github.com>
2021-06-09 14:29:11 +0700

    Nop bai tap

---
+++ src/xuLy.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class xuLy {
+    public static void main(String[] args) {
+        int m, n;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào số dòng của ma trận: ");
+        m = scanner.nextInt();
+        System.out.println("Nhập vào số cột của ma trận: ");
+        n = scanner.nextInt();
+
+        int arr[][] = new int[m][n];
+
+        System.out.println("Nhập các phần tử cho ma trận: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Max in array: " +maxValue(arr,m,n));
+    }
+
+    public static int maxValue(int arr[][], int m, int n) {
+        int max = arr[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}
